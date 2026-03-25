9c164ffe88c80cdf23588ed7d1a3e8289bcbaf23
Nafargi Damena <nafargidamena@gmail.com>
2026-03-25 22:21:58 +0300

    prepare deploy

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+  base: '/hamajispixel/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
